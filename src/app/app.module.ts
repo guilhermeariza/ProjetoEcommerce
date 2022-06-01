03ea0aee6a2d9b2810ec457b14f669e0a0c8edef
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-06-01 13:02:12 -0300

    Adicionado card de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardPesquisaProdutoComponent } from './card-pesquisa-produto/card-pesquisa-produto.component';
+import { CardLoginComponent } from './card-login/card-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CardPesquisaProdutoComponent
+    CardPesquisaProdutoComponent,
+    CardLoginComponent
   ],
   imports: [
     BrowserModule,
