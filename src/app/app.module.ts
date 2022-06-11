d71d2b5040c9b2640c96f36de2a5ee841264c739
Guilherme Ariza <gui.ariza@gmail.com>
2022-06-10 21:00:16 -0300

    criação da pasta components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CardPesquisaProdutoComponent } from './card-pesquisa-produto/card-pesquisa-produto.component';
-import { CardLoginComponent } from './card-login/card-login.component';
-import { CardHomeComponent } from './card-home/card-home.component';
-import { FooterComponent } from './footer/footer.component';
-import { CadastrarProdutoComponent } from './cadastrar-produto/cadastrar-produto.component';
-import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { CadastrarProdutoComponent } from './components/cadastrar-produto/cadastrar-produto.component';
+import { CardHomeComponent } from './components/card-home/card-home.component';
+import { CardLoginComponent } from './components/card-login/card-login.component';
+import { CardPesquisaProdutoComponent } from './components/card-pesquisa-produto/card-pesquisa-produto.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
 
 
 
