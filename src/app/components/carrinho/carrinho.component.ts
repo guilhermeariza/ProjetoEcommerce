97d58c6ed068922b7efd8f3f0849936ecbc93d55
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-06-14 15:41:23 -0300

    Conexao com api externa para armazenamento de imagens

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Carrinho } from 'src/app/model/Carrinho';
-import { Produto } from 'src/app/model/Produto';
-import { CarrinhoService } from 'src/app/service/carrinho.service';
-import { ProdutoService } from 'src/app/service/produto-service.service';
 
 @Component({
   selector: 'app-carrinho',
@@ -11,13 +7,11 @@ import { ProdutoService } from 'src/app/service/produto-service.service';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
-  carrinho: Carrinho[]=[]
-  produto: Produto[]=[]
 
-  constructor(private router: Router, private carrinhoService: CarrinhoService) { }
+  constructor(private router: Router) { }
 
   ngOnInit(){
-    this.carrinho=this.carrinhoService.getAll()
+
   }
 
 }
