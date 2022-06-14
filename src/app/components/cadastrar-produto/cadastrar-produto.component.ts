3dd0065670542d0b4fda50c194b4a51dc03475e3
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-06-14 15:38:02 -0300

    Conexao com api externa para armazenamento de imagens

--- src/app/components/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/components/cadastrar-produto/cadastrar-produto.component.ts
@@ -36,4 +36,39 @@ export class CadastrarProdutoComponent implements OnInit {
     $('#idProdutoExcluir').text(this.produto[i].id)
     $('#nomeProdutoExcluir').text(this.produto[i].nome)
   }
+
+  fileChange(event: Event) {
+    var file: any
+    if(file !== null){
+      file = document.getElementById('input_img');
+      var form = new FormData();
+      form.append("image", file.files[0]);
+      var settings= new Array
+      settings = [{
+          url: "https://api.imgbb.com/1/upload?key=f7dc66d97778642fac1278cb89831b02",
+          method: "POST",
+          timeout: 0,
+          processData: false,
+          mimeType: "multipart/form-data",
+          contentType: false,
+          data: form
+      }]
+      $.ajax(settings[0]).done(function (response) {
+          console.log(response);
+          var jx = JSON.parse(response);
+          var linkFoto = jx.data.url;
+          $('#setFoto').attr('src',linkFoto)
+          $('#input_img').hide()
+      });
+    }
+  }
+
+  limparImagem(){
+    $('#input_img').show()
+    $('#setFoto').attr('src','')
+  }
+
+  alerta(){
+    alert('esta funcionando')
+  }
 }
