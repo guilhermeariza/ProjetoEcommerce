75863f9da84e696926afdc28764480345b39ec60
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-06-19 16:09:09 -0300

    Service de cartao de credito com os metodos delete e getAll

--- src/app/service/cartao-credito.service.ts
+++ src/app/service/cartao-credito.service.ts
@@ -1,31 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { CartaoCredito } from '../model/CartaoCredito';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartaoCreditoService {
-  cartaoCredito: CartaoCredito[]=[
-    {
-      id: 1,
-      apelido: 'Cartao 1',
-      nomeCartao: 'Vinicius Campanholi',
-      numeroCartao: '4319771301916890',
-      dataValidade: '18/02/2025',
-      cvv: '101'
-    },
-    {
-      id: 2,
-      apelido: 'Cartao 2',
-      nomeCartao: 'Guilherme Ariza',
-      numeroCartao: '4969722216061572',
-      dataValidade: '11/12/2026',
-      cvv: '538'
-    },
-  ]
-  constructor() { }
 
-  getAll(){
-    return this.cartaoCredito
+  url: string = 'http://localhost/cartaocredito'
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Autorization', environment.token)
+  }
+
+  getAll(): Observable<CartaoCredito>{
+    return this.http.get<CartaoCredito>(this.url)
+  }
+
+  delete(cartaoCredito: CartaoCredito):Observable<CartaoCredito>{
+    return this.http.delete<CartaoCredito>(this.url +'/'+ cartaoCredito.id)
   }
 }
